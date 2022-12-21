aa7250d94d39d801fa48c701e63f549008bdc452
kristifourie <24703907>
2022-12-21 21:38:52 +0400

    rna to protein function added

---
+++ solutions/rna_to_protein.py
@@ -0,0 +1,30 @@
+#this function translates an RNA sequence into the corresponding protein sequence 
+def rna_to_protein(RNA_strand):
+    
+    #this map has all the translations
+    table = {"UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
+       "UCU":"S", "UCC":"S", "UCA":"S", "UCG":"S",
+       "UAU":"Y", "UAC":"Y", "UAA":"STOP", "UAG":"STOP",
+       "UGU":"C", "UGC":"C", "UGA":"STOP", "UGG":"W",
+       "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
+       "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
+       "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
+       "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
+       "AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
+       "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
+       "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
+       "AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
+       "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
+       "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
+       "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
+       "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G",}
+
+    protein = ""
+    if len(RNA_strand)%3 == 0:
+        for i in range(0, len(RNA_strand), 3):
+            codon = RNA_strand[i: i + 3]
+            protein+= table[codon]
+    print(protein)
+    return protein
+
+
